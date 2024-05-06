2553e6d3d7cb0a7a0eee267dc4c76100f7628fc0
Oscar Benjamin <oscar.j.benjamin@gmail.com>
2024-05-06 12:23:06 +0100

    Make setuptools coverage work

---
+++ coverage_setuptools.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+python setup.py build_ext --inplace
+PYTHONPATH=src pytest --cov=stuff --pyargs stuff.test
--- setup.py
+++ setup.py
@@ -3,14 +3,21 @@ from setuptools.extension import Extension
 from Cython.Distutils import build_ext
 from Cython.Build import cythonize
 
-ext = Extension(
+packages = [
     'stuff',
-    ['src/stuff.pyx'],
+]
+
+ext = Extension(
+    'stuff.thing',
+    ['src/stuff/thing.pyx'],
     define_macros = [('CYTHON_TRACE', 1)],
 )
 
+ext_modules = cythonize([ext], compiler_directives={'linetrace': True})
+
 setup(
     cmdclass={'build_ext': build_ext},
-    ext_modules=cythonize([ext], compiler_directives={'linetrace': True}),
+    ext_modules=ext_modules,
+    packages=packages,
     package_dir={'': 'src'},
 )
